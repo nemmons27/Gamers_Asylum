cb2f67600ead75ed603d0f3ed43c026ad53bd22b
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-07-12 13:43:13 -0400

    Genre Page built out

--- client/src/pages/Genre.jsx
+++ client/src/pages/Genre.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { GetGames } from '../services/GameServices'
+import { useNavigate } from 'react-router-dom'
 
 const Genre = () => {
+    let navigate = useNavigate()
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        const handleGames = async () => {
+            const data = await GetGames()
+            setGames(data)
+        }
+        handleGames()
+    }, [])
+
     return (
-        <p>Adventure</p>
+        <div className="grid col=4">
+            {games.map((game) => (
+                <div className='card' key={GetGames.id}>
+                    <h3>{game.name}</h3>
+                    <p>{game.genre}</p>
+                    <p>{game.description}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
